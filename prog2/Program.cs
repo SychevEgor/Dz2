8b53a1f71c4625e1e63e4a82cafab61191ddb2ba
Егор <tkuik255@gmail.com>
2022-09-22 18:56:11 +0300

    сохранение

---
+++ prog2/Program.cs
@@ -0,0 +1,14 @@
+﻿int result = -1;
+int number = new Random().Next(10,99999);
+Console.WriteLine($"случайное число :{number}");
+while (number > 99)
+{
+    result = number % 10;
+    number = number/10;
+
+}
+if (result >= 0)
+{
+ Console.WriteLine($"Третье число: {result}");
+}
+else Console.WriteLine("Треетей цифры нет");
---
+++ prog3/Program.cs
@@ -0,0 +1,4 @@
+﻿int day = new Random().Next(1,8);
+Console.Write($"День {day}\n");
+if (day>5) Console.WriteLine("да");
+else Console.WriteLine ("нет");
